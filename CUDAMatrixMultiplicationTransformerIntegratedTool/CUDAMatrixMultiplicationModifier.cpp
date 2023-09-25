cd05597a65293b475f78fd25884509b65b30a7a2
GÖKAY GÜLSOY <111280608+GokayGulsoy@users.noreply.github.com>
2023-09-25 13:49:57 +0300

    CUDA matrix multiplication modifier integrated tool added

---
+++ CUDAMatrixMultiplicationTransformerIntegratedTool/CUDAMatrixMultiplicationModifier.cpp
@@ -0,0 +1,140 @@
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendAction.h"
+#include "clang/Rewrite/Core/Rewriter.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+#include "llvm/Support/CommandLine.h"
+
+using namespace clang;
+using namespace clang::tooling;
+using namespace llvm;
+
+static llvm::cl::OptionCategory MyToolCategory("my-tool options");
+
+static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
+
+cl::opt<std::string>
+    ThreadsValue("threads",
+                 cl::desc("Specify the value to replace THREADS with"),
+                 cl::value_desc("value"), cl::init("64"));
+
+cl::opt<bool>
+    ConvertDoubleToFloat("convert-double-to-float",
+                         cl::desc("Convert double variables to float."),
+                         cl::init(false));
+
+class MyASTVisitor : public RecursiveASTVisitor<MyASTVisitor> {
+public:
+  explicit MyASTVisitor(ASTContext *Context, Rewriter &R)
+      : Context(Context), R(R) {}
+
+  // Visit variable declarations and change the THREADS value.
+  bool VisitVarDecl(VarDecl *VD) {
+    if (VD->getNameAsString() == "THREADS") {
+      // Replace the THREADS value with the user-specified value.
+      if (VD->hasInit()) {
+        Expr *InitExpr = VD->getInit();
+        SourceRange ReplacementRange = InitExpr->getSourceRange();
+        StringRef ReplacementText = ThreadsValue;
+        R.ReplaceText(ReplacementRange, ReplacementText);
+      }
+    }
+    return true;
+  }
+
+  // Visit type declarations and replace double with float.
+  bool VisitTypeLoc(TypeLoc TL) {
+    if (ConvertDoubleToFloat) {
+
+      QualType QT = TL.getType();
+      if (QT->isSpecificBuiltinType(BuiltinType::Double)) {
+        // Replace the type "double" with "float".
+        SourceRange ReplacementRange = TL.getSourceRange();
+        StringRef ReplacementText = "float";
+
+        // Calculate the correct replacement length to ensure adjacent words are
+        // separated.
+        size_t OriginalLength = R.getRangeSize(ReplacementRange);
+        size_t NewLength = ReplacementText.size();
+        if (OriginalLength != NewLength) {
+          // Adjust the end location to match the new length.
+          SourceLocation EndLoc = ReplacementRange.getBegin().getLocWithOffset(
+              OriginalLength - NewLength);
+          ReplacementRange.setEnd(EndLoc);
+        }
+
+        // Replace and print a message indicating the type conversion.
+        R.ReplaceText(ReplacementRange, ReplacementText);
+      }
+    }
+    
+    return true;
+  }
+
+private:
+  ASTContext *Context;
+  Rewriter &R;
+};
+
+class MyASTConsumer : public ASTConsumer {
+public:
+  explicit MyASTConsumer(ASTContext *Context, Rewriter &R)
+      : Visitor(Context, R), TheRewriter(R) {}
+
+  void HandleTranslationUnit(ASTContext &Context) override {
+    // Visit and transform variable declarations.
+    Visitor.TraverseDecl(Context.getTranslationUnitDecl());
+
+    // Print the modified code directly from the Rewriter.
+    TheRewriter.getEditBuffer(Context.getSourceManager().getMainFileID())
+        .write(llvm::outs());
+  }
+
+private:
+  MyASTVisitor Visitor;
+  Rewriter &TheRewriter;
+};
+
+class MyFrontendAction : public ASTFrontendAction {
+public:
+  MyFrontendAction() {}
+
+  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &Compiler,
+                                                 StringRef InFile) override {
+    TheRewriter.setSourceMgr(Compiler.getSourceManager(),
+                             Compiler.getLangOpts());
+    return std::make_unique<MyASTConsumer>(&Compiler.getASTContext(),
+                                           TheRewriter);
+  }
+
+private:
+  Rewriter TheRewriter;
+};
+
+int main(int argc, const char **argv) {
+  auto ExpectedParser = CommonOptionsParser::create(argc, argv, MyToolCategory);
+  if (!ExpectedParser) {
+    llvm::errs() << ExpectedParser.takeError();
+    return 1;
+  }
+
+  CommonOptionsParser &op = ExpectedParser.get();
+  ClangTool Tool(op.getCompilations(), op.getSourcePathList());
+
+  // Create a FrontendActionFactory for MyFrontendAction.
+  auto Factory = newFrontendActionFactory<MyFrontendAction>();
+
+  // Run the tool with the FrontendActionFactory.
+  int Result = Tool.run(Factory.get());
+
+  if (Result != 0) {
+    llvm::errs() << "Error occurred while running the tool.\n";
+    return Result;
+  }
+
+  return 0;
+}
